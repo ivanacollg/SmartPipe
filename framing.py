4e408682f4a466fb3383f68240ae4405a500a99e
Roberto Ramírez Monroy <38079022+rbtote@users.noreply.github.com>
2019-11-07 01:05:41 -0600

    Update framing.py

--- framing.py
+++ framing.py
@@ -10,19 +10,23 @@ import numpy as np
 import os
 
 
-file = 'SEQ_0490'
+file = 'SEQ_0473'
 fileType = ".wmv"
 hasLeak = True
 width = 320
 height = 240
 # set video file path of input video with name and extension
-vid = cv2.VideoCapture(file+'.wmv')
+vid = cv2.VideoCapture(file+fileType)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
-if not os.path.exists(file):
-    os.makedirs(file)
-
-f = open('./'+file+'/'+file+".txt", "w")   # 'r' for reading and 'w' for writing
+if hasLeak:
+    if not os.path.exists(file+"_fuga"):
+        os.makedirs(file+"_fuga")
+    f = open('./'+file+'_fuga/'+file+".txt", "w")   # 'r' for reading and 'w' for writing
+else:
+    if not os.path.exists(file+"_nofuga"):
+        os.makedirs(file+"_nofuga")
+    f = open('./'+file+'_nofuga/'+file+".txt", "w")   # 'r' for reading and 'w' for writing
 
 #for frame identity
 index = 0
@@ -33,18 +37,17 @@ while(True):
     # end of frames
     if not ret: 
         f.close()   
-        break
-    # Saves images
-    name = './'+file+'/'+file+'_' + str(index) + '.jpg'
+        break    
+    if hasLeak:
+        name = './'+file+'_fuga/'+file+'_' + str(index) + '.jpg' 
+        f.write("1 " + file+'_' + str(index) + '.jpg\n')    # Write inside file 
+    else:
+        name = './'+file+'_nofuga/'+file+'_' + str(index) + '.jpg' 
+        f.write("0 " + file+'_' + str(index) + '.jpg\n')    # Write inside file 
     print ('Creating...' + name)
     fgmask = cv2.bitwise_not(fgmask)
     dim = (width, height)
-    # resize image
     resized = cv2.resize(fgmask, dim, interpolation = cv2.INTER_AREA)
     cv2.imwrite(name, resized)
-    if hasLeak:
-        f.write("1 " + file+'_' + str(index) + '.jpg\n')    # Write inside file 
-    else:
-        f.write("0 " + file+'_' + str(index) + '.jpg\n')    # Write inside file 
     # next frame
     index += 1
