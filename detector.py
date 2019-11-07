e173146d150cedb053957bbd8032f2e562fd21bf
ivanacoll <ivanacollado@gmail.com>
2019-11-07 17:27:51 +0100

    Layer architecture update

--- detector.py
+++ detector.py
@@ -1,125 +1,81 @@
 import numpy as np
-import mnist
 import keras
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
-from keras.utils import to_categorical
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
+import matplotlib as plt
+
+
+#Data augmentation y subir dataset
+#Se requieren dos carpetas: training_set y test_set. Dentro de cada una, van dos carpetas, una por cada clase.
 from keras.preprocessing.image import ImageDataGenerator
-import tensorflow as tf
-import pathlib
 
+train_datagen = ImageDataGenerator(rescale = 1./255-0.5,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
 
-data_dir = pathlib.Path('/images')
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
+test_datagen = ImageDataGenerator(rescale = 1./255-0.5)
 
+train_set = train_datagen.flow_from_directory('images/train',
+                                                 target_size = (240, 320), #Tamano de la imagen
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
 
-list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'))
-for f in list_ds.take(5):
-  print(f.numpy())
-
-
-
-#Data Preparation
-train_path = '/train'
-valid_path = '/valid'
-test_path = '/test'
-data_dir = '/test'
-
-
-train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(240,320), classes=['fuga','no_fuga'], batch_size = 10)
-valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(240,320), classes=['fuga','no_fuga'], batch_size = 4)
-test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(240,320), classes=['fuga','no_fuga'], batch_size = 4)
-
-
-# plots images with labels within jupyter notebook
-def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
-    if type(ims[0]) is np.ndarray:
-        ims = np.array(ims).astype(np.uint8)
-        if (ims.shape[-1] != 3):
-            ims = ims.transpose((0,2,3,1))
-    f = plt.figure(figsize=figsize)
-    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
-    for i in range(len(ims)):
-        sp = f.add_subplot(rows, cols, i+1)
-        sp.axis('Off')
-        if titles is not None:
-            sp.set_title(titles[i], fontsize=16)
-        plt.imshow(ims[i], interpolation=None if interp else 'none')
-
-
-imgs, labels = next(train_batches)
-print(imgs)
-print(labels)
-
-plots(imgs, titles=labels)
-
+test_set = test_datagen.flow_from_directory('images/test',
+                                            target_size = (240, 320), #Tamano de la imagen
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+print(train_set) # (60000, 28, 28)
+print(train_set) # (60000,)
 
 '''
-# The first time you run this might be a bit slow, since the
-# mnist package has to download and cache the data.
-train_images = mnist.train_images()
-train_labels = mnist.train_labels()
-
-print(train_images.shape) # (60000, 28, 28)
-print(train_labels.shape) # (60000,)
-
-train_images = mnist.train_images()
-train_labels = mnist.train_labels()
-test_images = mnist.test_images()
-test_labels = mnist.test_labels()
-
-# Normalize the images.
-train_images = (train_images / 255) - 0.5
-test_images = (test_images / 255) - 0.5
-
 # Reshape the images.
-train_images = np.expand_dims(train_images, axis=3)
-test_images = np.expand_dims(test_images, axis=3)
+train_set = np.expand_dims(train_set, axis=3)
+test_set= np.expand_dims(test_set, axis=3)
 
-print(train_images.shape) # (60000, 28, 28, 1)
-print(test_images.shape)  # (10000, 28, 28, 1)
-
-num_filters = 8
-filter_size = 3
-pool_size = 2
-
-# Build the model.
-model = Sequential([
-  Conv2D(num_filters, filter_size, input_shape=(28, 28, 1)),
-  MaxPooling2D(pool_size=pool_size),
-  Flatten(),
-  Dense(10, activation='softmax'),
-])
-
-
-# Compile the model.
-model.compile(
-  'adam',
-  loss='categorical_crossentropy',
-  metrics=['accuracy'],
-)
-
-# Train the model.
-model.fit(
-  train_images,
-  to_categorical(train_labels),
-  epochs=3,
-  validation_data=(test_images, to_categorical(test_labels)),
-)
-
-# Save the model to disk.
-model.save_weights('cnn.h5')
-
-# Load the model from disk later using:
-# model.load_weights('cnn.h5')
-
-# Predict on the first 5 test images.
-predictions = model.predict(test_images[:5])
-
-# Print our model's predictions.
-print(np.argmax(predictions, axis=1)) # [7, 2, 1, 0, 4]
-
-# Check our predictions against the ground truths.
-print(test_labels[:5]) # [7, 2, 1, 0, 4]
+print(train_set.shape) # (60000, 28, 28, 1)
+print(test_set.shape)  # (10000, 28, 28, 1)
 '''
+
+
+# Initialising the CNN
+classifier = Sequential()
+
+#  Conv-Pool structure 1
+classifier.add(Conv2D(4, (3, 3), input_shape = (240, 320,3), activation = 'relu'))
+classifier.add(Dropout(0.2))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+
+# Conv-Pool structure 2
+classifier.add(Conv2D(8, (3, 3), activation = 'relu'))
+classifier.add(Dropout(0.2))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+
+classifier.add(Flatten())
+#Full connection
+classifier.add(Dense(units = 2400, activation = 'relu'))
+classifier.add(Dense(units = 32, activation = 'softmax'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+classifier.summary()
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+classifier.fit(train_set, epochs=3, validation_data=(test_set))
+
+'''
+plt.plot(classifier.history['accuracy'], label='accuracy')
+plt.plot(classifier.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+'''
+
+test_loss, test_acc = classifier.evaluate(test_set, verbose=2)
+
+
