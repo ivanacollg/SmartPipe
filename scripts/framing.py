a2d2c6330eea24603d582b05c1898f03454a331f
Roberto Ramírez Monroy <38079022+rbtote@users.noreply.github.com>
2019-11-13 19:46:16 -0600

    Update framing.py

--- scripts/framing.py
+++ scripts/framing.py
@@ -8,26 +8,23 @@ Created on Wed Nov  6 22:28:33 2019
 import cv2
 import numpy as np
 import os
-from pathlib import Path
 
 
-file = 'SEQ_0486'
+file = 'SEQ_0478'
 fileType = ".wmv"
 hasLeak = False
+pathImg = "../images/train/"
+pathVid = "../videos/"
 width = 320
 height = 240
 # set video file path of input video with name and extension
-vid = cv2.VideoCapture('../videos/'+file+fileType)
+vid = cv2.VideoCapture(pathVid+file+fileType)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 if hasLeak:
-    if not os.path.exists("../dataset/"+file+"_fuga"):
-        os.makedirs("../dataset/"+file+"_fuga")
-    #f = open('./dataset/'+file+'_fuga/'+file+".txt", "w")   # 'r' for reading and 'w' for writing
+    pathImg=pathImg+"fuga/"
 else:
-    if not os.path.exists("../dataset/"+file+"_nofuga"):
-        os.makedirs("../dataset/"+file+"_nofuga")
-    #f = open('./dataset/'+file+'_nofuga/'+file+".txt", "w")   # 'r' for reading and 'w' for writing
+    pathImg=pathImg+"nofuga/"    
 
 #for frame identity
 index = 0
@@ -36,20 +33,14 @@ while(True):
     ret, frame = vid.read()
     fgmask = fgbg.apply(frame)    
     # end of frames
-    if not ret: 
-        #f.close()   
+    if not ret:
         break    
-    if hasLeak:
-        name = '../dataset/'+file+'_fuga/'+file+'_' + str(index) + '.jpg' 
-        #f.write("1 " + file+'_' + str(index) + '.jpg\n')    # Write inside file 
-    else:
-        name = '../dataset/'+file+'_nofuga/'+file+'_' + str(index) + '.jpg' 
-        #f.write("0 " + file+'_' + str(index) + '.jpg\n')    # Write inside file 
+    name = pathImg+file+'_' + str(index) + '.jpg'                 
     print(name)    
     print ('Creating...' + name)
     fgmask = cv2.bitwise_not(fgmask)
     dim = (width, height)
-    resized = cv2.resize(fgmask, dim, interpolation = cv2.INTER_AREA)    
+    resized = cv2.resize(fgmask, dim, interpolation = cv2.INTER_AREA)
     cv2.imwrite(name, resized)
     # next frame
     index += 1
